fd418b250d9dd09c2f0dbfdfd583a888147bf82a
elkin <elkin@DESKTOP-KQKAO9U>
2025-04-25 14:39:54 +0200

    Cambio hola

--- src/prueba2/Prueba2.java
+++ src/prueba2/Prueba2.java
@@ -15,6 +15,9 @@ public class Prueba2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    System.out.println("Hola");
+    
+    
     }
     
 }
